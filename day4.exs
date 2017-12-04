a9f300e173698e291c6b48ba43cff01aea0c40f0
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-12-04 17:27:13 -0500

    Day 4 part 2 in elixir

--- day4.exs
+++ day4.exs
@@ -1,11 +1,29 @@
-File.read!("day4_input.txt")
-  |> String.split(~r{\n})
-  |> Enum.reduce(0, fn(x, acc) ->
+defmodule Day4 do
+  def part1(x, acc) do
     l = String.split(x)
-
     case length(l) == length(Enum.uniq(l)) and length(l) > 0 do
       true ->  acc + 1
       _ -> acc
     end
-  end)
+  end
+
+  def part2(x, acc) do
+    l = String.split(x) |> Enum.map(fn x -> x |> String.graphemes |> Enum.sort |> Enum.join end)
+    case length(l) == length(Enum.uniq(l)) and length(l) > 0 do
+      true ->  acc + 1
+      _ -> acc
+    end
+  end
+end
+
+# Part 1
+File.read!("day4_input.txt")
+  |> String.split(~r{\n})
+  |> Enum.reduce(0, &Day4.part1/2)
+  |> IO.inspect
+
+# Part 2
+File.read!("day4_input.txt")
+  |> String.split(~r{\n})
+  |> Enum.reduce(0, &Day4.part2/2)
   |> IO.inspect
