1a5679f9f54cff4ab09e6d8009068bb5f706eccb
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-12-14 00:14:24 -0500

    day 13. part 2 and an improved part 1

--- 13/day13.exs
+++ 13/day13.exs
@@ -1,27 +1,17 @@
 defmodule Day13 do
-  def scan(firewall) do
-    firewall
-    |> Enum.reduce(firewall, &move/2)
-  end
-
-  defp move({k, %{range: r, pos: p, dir: :down} = v}, acc) when p == r do
-    acc
-    |> Map.put(k, Map.merge(v, %{pos: p - 1, dir: :up}))
-  end
-
-  defp move({k, %{range: _r, pos: p, dir: :down} = v}, acc) do
-    acc
-    |> Map.put(k, Map.merge(v, %{pos: p + 1}))
-  end
-
-  defp move({k, %{range: _r, pos: p, dir: :up} = v}, acc) when p == 1 do
-    acc
-    |> Map.put(k, Map.merge(v, %{pos: p + 1, dir: :down}))
-  end
-
-  defp move({k, %{range: _r, pos: p, dir: :up} = v}, acc) do
-    acc
-    |> Map.put(k, Map.merge(v, %{pos: p - 1}))
+  def traverse(input, delay) do
+    0..(Map.keys(input) |> Enum.max)
+    |> Enum.reduce({0, 0}, fn(x, {acc, acc2}) ->
+      {zap, range} = case input[x] do
+        nil -> {nil, nil}
+        r -> {rem(x + delay, 2 * (r - 1)) == 0, r}
+      end
+      {acc, acc2} = case zap do
+        true -> {acc + x * range, acc2 + 1}
+        _ -> {acc, acc2}
+      end
+      {acc, acc2}
+    end)
   end
 end
 
@@ -29,18 +19,18 @@ input = File.read!("day13_input.txt")
 |> String.split("\n", trim: true)
 |> Enum.reduce(%{}, fn(x, acc) ->
   [a, b] = String.split(x, ":")
-  Map.put(acc, String.to_integer(a), %{range: String.trim(b) |> String.to_integer, pos: 1, dir: :down})
+  Map.put(acc, String.to_integer(a), String.trim(b) |> String.to_integer)
 end)
 
-{severity, _f} =
-  0..(Map.keys(input) |> Enum.max)
-  |> Enum.reduce({0, input}, fn(x, {acc, firewall}) ->
-    scanner = firewall[x]
-    acc = case scanner[:pos] do
-      1 -> acc + x * scanner[:range]
-      _ -> acc
-    end
-    {acc, Day13.scan(firewall)}
-  end)
+{s, _z} = Day13.traverse(input, 0) # Part 1
+IO.inspect s
 
-IO.inspect severity
+1..9000000 # Part 2
+|> Enum.reduce_while(0, fn(x, _acc) ->
+  {_s, z} = Day13.traverse(input, x)
+  cond do
+    z == 0 -> {:halt, x}
+    true -> {:cont, x}
+  end
+end)
+|> IO.inspect
