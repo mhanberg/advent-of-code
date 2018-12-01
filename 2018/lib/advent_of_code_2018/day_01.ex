9b754b5a1e3006f0071b056539a4018d8a39f628
Mitchell Hanberg <mitch@mitchellhanberg.com>
2018-12-01 03:02:12 -0500

    Optimize day 1
    Using a Map is always more efficient than an list when it comes to look
    up time.

--- 2018/lib/advent_of_code_2018/day_01.ex
+++ 2018/lib/advent_of_code_2018/day_01.ex
@@ -7,7 +7,7 @@ defmodule AdventOfCode2018.Day01 do
   end
 
   def part2(args) do
-    find_dup(args, {0, []}, args)
+    find_dup(args, {0, Map.new}, args)
   end
 
   defp find_dup([], acc, full_args) do
@@ -18,10 +18,10 @@ defmodule AdventOfCode2018.Day01 do
     [x | tail] = args
     new_acc = acc + String.to_integer(x)
 
-    if Enum.member?(cycle_list, new_acc) do
+    if Map.has_key?(cycle_list, new_acc) do
       new_acc
     else
-      new_cycle_list = [new_acc | cycle_list]
+      new_cycle_list = Map.put(cycle_list, new_acc, new_acc)
 
       find_dup(tail, {new_acc, new_cycle_list}, full_args)
     end
